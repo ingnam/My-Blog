5a7cb11cb7ef13455fa6d9a5059ec2cf0eb1f9e9
Madan Ingnam <madingnm@gmail.com>
2014-12-13 18:56:02 +0545

    Edit and Delete post features created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,25 @@ class PostsController < ApplicationController
 		end	
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post=Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end	
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
 	#Rails 4 has a security feature called strong parameters
 	private
 	def post_params
